b6a903f93c67940471dbd9c2d73b0405bcda7ec7
Raffael Schmid <raffael.schmid@cloudarchitects.ch>
2021-10-31 07:38:36 +0000

    -

--- preprocessing/text.py
+++ preprocessing/text.py
@@ -4,3 +4,16 @@ def tokenize(text):
     return [word.lower() for word in word_tokenize(text)]
 
 
+def is_iterable(obj):
+    
+    try:
+        iter(obj)
+        return True
+    except TypeError:
+        return False
+
+def join(tokens):
+    return " ".join(tokens) if is_iterable(tokens) else ""
+
+
+
