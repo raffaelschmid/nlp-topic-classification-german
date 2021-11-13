8747f5eff025c954598524574cd75eaf64d903c5
Raffael Schmid <raffael.schmid@cloudarchitects.ch>
2021-11-13 07:08:22 +0000

    -

--- preprocessing/text.py
+++ preprocessing/text.py
@@ -17,3 +17,24 @@ def join(tokens):
 
 
 
+
+def extract_vocabulary(data, verbose=False, sequence_length_percentil_cutoff = 0.98, sequence_length_max = 768):
+
+    vocabulary = set()
+    _ = data.apply(lambda x: vocabulary.update(x))
+
+    lengths = data.apply(len)
+    max_sequence_length = int(lengths.quantile(1.0))
+    percentil_sequence_length = int(lengths.quantile(0.98))
+    median_sequence_length = int(lengths.quantile(0.5))
+    embedding_length = min(sequence_length_max, percentil_sequence_length)
+
+    if verbose:
+        print(f"Median sequence length:       : {median_sequence_length}")
+        print(f"Percentil                     : {sequence_length_percentil_cutoff})")
+        print(f"Cutoff sequence length        : {percentil_sequence_length}")
+        print(f"Max sequence length           : {max_sequence_length}")
+        print(f"Embedding length              : {embedding_length}")
+        print(f"Vocabulary length             : {len(vocabulary)}")
+
+    return (vocabulary, embedding_length)
