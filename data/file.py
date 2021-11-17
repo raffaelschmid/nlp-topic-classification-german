d5b08b1a9b3b5be36c494f3a272a773b35d9807c
Raffael Schmid <raffael.schmid@students.fhnw.ch>
2021-11-17 06:39:04 +0000

    split validation dataset

--- data/file.py
+++ data/file.py
@@ -4,5 +4,8 @@ data_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.abspath(f"{data_dir}/..")
 news_articles_raw_train = f"{root_dir}/data/raw/german_news_articles_raw_train.parq"
 news_articles_raw_test = f"{root_dir}/data/raw/german_news_articles_raw_test.parq"
+
+news_articles_cleaned = f"{root_dir}/data/processed/german_news_articles_cleaned.parq"
 news_articles_cleaned_train = f"{root_dir}/data/processed/german_news_articles_cleaned_train.parq"
 news_articles_cleaned_test = f"{root_dir}/data/processed/german_news_articles_cleaned_test.parq"
+news_articles_cleaned_val = f"{root_dir}/data/processed/german_news_articles_cleaned_val.parq"
