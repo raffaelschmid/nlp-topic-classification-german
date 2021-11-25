3e635bf751f654a822ffd635318371e28ca8d86b
Raffael Schmid <raffael.schmid@cloudarchitects.ch>
2021-11-25 09:26:44 +0100

    evaluation

--- reporting/evaluation.py
+++ reporting/evaluation.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from pandas import DataFrame
-from  seaborn import heatmap
+from seaborn import heatmap
 from sklearn.metrics import confusion_matrix
 
-def plot_confusion_matrix(y_true, y_pred, figsize=(10,10)):
-    
+
+def plot_confusion_matrix(y_true, y_pred, figsize=(10, 10)):
+    """
+    Plots confusion matrix containing percentage
+    """
     cm = confusion_matrix(y_true, y_pred, labels=np.unique(y_true))
     cm_sum = np.sum(cm, axis=1, keepdims=True)
     cm_perc = cm / cm_sum.astype(float) * 100
@@ -26,4 +29,24 @@ def plot_confusion_matrix(y_true, y_pred, figsize=(10,10)):
     cm.index.name = 'Actual'
     cm.columns.name = 'Predicted'
     fig, ax = plt.subplots(figsize=figsize)
-    heatmap(cm, cmap= "YlGnBu", annot=annot, fmt='', ax=ax)
+    heatmap(cm, cmap="YlGnBu", annot=annot, fmt='', ax=ax)
+
+
+def percentage_confusion_matrix(y_pred, y_true):
+    """
+    Returns confusion matrix percentage values.
+    :rtype: DataFrame
+    """
+    cm = confusion_matrix(y_true, y_pred, labels=np.unique(y_true))
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = np.round(cm / cm_sum.astype(float) * 100, 3)
+
+    p = DataFrame(cm_perc, index=np.unique(y_true), columns=np.unique(y_true))
+    p.index.name = 'Actual'
+    p.columns.name = 'Predicted'
+    return p
+
+def percentage_true_positives(predicted, expected, column='True Positives'):
+    perc = percentage_confusion_matrix(predicted, expected)
+    data = [perc[label][label] for label in perc.index]
+    return DataFrame(data, index=np.unique(expected), columns=[column])
